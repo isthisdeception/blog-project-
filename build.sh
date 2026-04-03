939e28dd05a437f00dcef01744e85a60c693977c
Assaduzzaman <md.assaduzzaman99@gmail.com>
2026-04-03 22:11:31 +0600

    remove supuerusercreation scripts

--- build.sh
+++ build.sh
@@ -10,15 +10,3 @@ python manage.py collectstatic --no-input
 
 # Apply database migrations
 python manage.py migrate
-
-python manage.py shell -c "
-from django.contrib.auth import get_user_model
-User = get_user_model()
-
-username = 'admin'
-email = 'admin@email.com'
-password = 'amdin'
-
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username, email, password)
-"
